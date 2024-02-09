c1c81717e2def33bea36518cc9f492c5aacde774
Vasilis Polyzos <bill_polyzos@hotmail.com>
2024-02-09 21:26:03 +0200

    created api caller and api  model

---
+++ Kaizen/Network/GenericAPICall.swift
@@ -0,0 +1,8 @@
+//
+//  GenericAPICall.swift
+//  Kaizen
+//
+//  Created by Bill on 9/2/24.
+//
+
+import Foundation
---
+++ Kaizen/Network/Models/SportsModel.swift
@@ -0,0 +1,8 @@
+//
+//  SportsModel.swift
+//  Kaizen
+//
+//  Created by Bill on 9/2/24.
+//
+
+import Foundation
