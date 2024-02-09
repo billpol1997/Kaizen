e6362952ebc576c83bc429cf3a1b0f6ed74fed82
Vasilis Polyzos <bill_polyzos@hotmail.com>
2024-02-09 21:28:26 +0200

    ...

--- Kaizen/Network/Models/SportsModel.swift
+++ Kaizen/Network/Models/SportsModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct SportsModel: Decodable {
+    let i: String? //id
+    let d: String? //name
+    let e: [EventsModel]? //events
+}
+
+struct EventsModel: Decodable {
+    let d: String? //name
+    let i: String? //id
+    let si: String? //sport id
+    let sh: String? // ??
+    let tt: Int? // event start time
+}
