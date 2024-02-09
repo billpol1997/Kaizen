31dc8e484f4f32811c138b80d1c48ada3b78e9ae
Vasilis Polyzos <bill_polyzos@hotmail.com>
2024-02-09 21:39:00 +0200

    ..

--- Kaizen/Network/Models/SportsList.swift
+++ Kaizen/Network/Models/SportsList.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct SportsList {
+    let list: [Sport]?
+    
+    init(list: [SportsModel]?) {
+        self.list = list.map({ sports in
+            sports.map { item in
+                Sport(sport: item)
+            }
+        })
+    }
+}
+
+struct Sport {
+    let id: String?
+    let name: String?
+    let events: [Event]?
+    
+    init(sport: SportsModel?) {
+        self.id = sport?.i
+        self.name = sport?.d
+        self.events = sport?.e.map({ events in
+            events.map { item in
+                Event(event: item)
+            }
+        })
+    }
+}
+
+struct Event {
+    let id: String?
+    let name: String?
+    let sportId: String?
+    let startTime: Int?
+    
+    init(event: EventsModel?) {
+        self.id = event?.i
+        self.name = event?.d
+        self.sportId = event?.si
+        self.startTime = event?.tt
+    }
+}
